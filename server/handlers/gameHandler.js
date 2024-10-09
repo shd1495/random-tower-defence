a814aed7f7f810f2228804ec8feb168a88ee2e86
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-09 17:51:34 +0900

    feat: "gameHandler 구현"
    gameStart 이벤트 함수 구현
    게임 시작 시 user 데이터 초기 검증 및 세팅

---
+++ server/handlers/gameHandler.js
@@ -0,0 +1,25 @@
+import { getGameAssets } from '../init/assets.js';
+
+export const gameStart = (uuid, payload, socket) => {
+  const { user } = getGameAssets();
+
+  const { userGold, baseHp, towerCost, numOfInitialTowers, monsterLevel, monsterSpawnInterval } =
+    user.data;
+
+  try {
+    socket.emit('gameStart', {
+      status: 'success',
+      userGold: userGold,
+      baseHp: baseHp,
+      towerCost: towerCost,
+      numOfInitialTowers: numOfInitialTowers,
+      monsterLevel: monsterLevel,
+      monsterSpawnInterval: monsterSpawnInterval,
+    });
+  } catch (error) {
+    socket.emit('gameStart', {
+      status: 'failure',
+      message: '게임 시작 데이터 로드에 실패했습니다.',
+    });
+  }
+};
