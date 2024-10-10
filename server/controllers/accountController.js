c79ebbcea3499dc82ca40b5f0564f5e50d841804
surinnnnnn <didthfls2@naver.com>
2024-10-10 12:22:24 +0900

    fix(양소린):game.html 분리 등
    fix(양소린):game.html 분리
    fix(양소린):로그인, 회원가입 fetch 추가
    fix(양소린):app.js 어카운트 라우터 추가
    fix(양소린):prisma 스키마 수정

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,8 +1,9 @@
-import { throwError } from '../utils/errorHandle.js';
-import accountService from '../services/accountService.js';
-import jwt from 'jsonwebtoken';
-import Joi from 'joi';
-import { v4 as uuidv4 } from 'uuid';
+import { throwError } from "../utils/errorHandle.js";
+import accountService from "../services/accountService.js";
+import jwt from "jsonwebtoken";
+import Joi from "joi";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
 
 /**
 //  * 회원가입
@@ -12,40 +13,56 @@ import { v4 as uuidv4 } from 'uuid';
 //  * @returns
 //  */
 
-export async function signup(req, res, next) {
-  const { accountId, password, confirmPassword } = req.body;
-  try {
-    // 데이터 유효성
-    const schema = Joi.object({
-      accountId: Joi.string()
-        .pattern(/^[a-zA-Z0-9]+$/)
-        .min(6)
-        .max(16)
-        .required(),
-      password: Joi.string().min(6).max(16).required(),
-      confirmPassword: Joi.valid(Joi.ref(`password`)).required(),
-    });
-    const { error } = schema.validate(req.body);
-    if (error) {
-      // error
-      return;
-    }
+export async function signup(req, res) {
+     try {
+          const { accountId, password, confirmPassword } = req.body;
+          console.log(req.body);
+          // 데이터 유효성 검사
+          const schema = Joi.object({
+               accountId: Joi.string()
+                    .pattern(/^[a-zA-Z0-9]+$/)
+                    .min(6)
+                    .max(16)
+                    .required(),
+               password: Joi.string().min(6).max(16).required(),
+               confirmPassword: Joi.valid(Joi.ref("password")).required(),
+          });
 
-    // 계정 찾기, 계정 있는지 확인
-    const existingAccount = await accountService.findAccount(accountId);
-    if (existingAccount) {
-      // error: 이미 id가 일치하는 계정이 존재하는 경우
-      return;
-    }
+          const { error } = schema.validate(req.body);
+          if (error) {
+               return res
+                    .status(400)
+                    .json({ message: error.details[0].message });
+          }
 
-    const uuid = uuidv4();
-    // HIGHLIGHT: 계정 추가 부분 uuid (계정 추가 부분 uuid)
-    const newAccount = await accountService.createAccount(accountId, password, uuid);
+          // 계정 찾기, 계정 있는지 확인
+          const existingAccount = await accountService.findAccount(accountId);
+          if (existingAccount) {
+               return res
+                    .status(400)
+                    .json({ message: "이미 사용 중인 계정 ID입니다." });
+          }
 
-    return res.status(201).json({ message: '회원 가입에 성공', account: newAccount });
-  } catch (error) {
-    next(error);
-  }
+          // 비밀번호 해시화
+          const hashedPassword = await bcrypt.hash(password, 10);
+
+          const uuid = uuidv4();
+          // HIGHLIGHT: 계정 추가 부분 uuid
+          const newAccount = await accountService.createAccount(
+               accountId,
+               hashedPassword,
+               uuid
+          );
+
+          return res
+               .status(201)
+               .json({ message: "회원 가입에 성공", account: newAccount });
+     } catch (error) {
+          console.error("회원가입 오류:", error);
+          return res
+               .status(500)
+               .json({ message: "서버에 문제가 발생했습니다." });
+     }
 }
 
 /**
@@ -56,55 +73,65 @@ export async function signup(req, res, next) {
  * @returns
  */
 export async function signin(req, res, next) {
-  const { accountId, password } = req.body;
-  try {
-    //데이터 유효성
-    const schema = Joi.object({
-      accountId: Joi.string()
-        .pattern(/^[a-zA-Z0-9]+$/)
-        .min(6)
-        .max(16)
-        .required(),
-      password: Joi.string().min(6).max(16).required(),
-    });
-    const { error } = schema.validate(req.body);
-    if (error) {
-      throwError(400, error.details[0].message);
-    }
+     try {
+          const { accountId, password } = req.body;
+          const schema = Joi.object({
+               accountId: Joi.string()
+                    .pattern(/^[a-zA-Z0-9]+$/)
+                    .min(6)
+                    .max(16)
+                    .required(),
+               password: Joi.string().min(6).max(16).required(),
+          });
+          const { error } = schema.validate(req.body);
+          if (error) {
+               return res
+                    .status(400)
+                    .json({ message: error.details[0].message });
+          }
 
-    // 존재하는 지, password는 일치하는지
-    const existingAccount = await accountService.findAccount(accountId);
-    if (!existingAccount) {
-      // error: 계정이 존재하지 않는 경우
-      return;
-    }
+          // 존재하는 지, password는 일치하는지
+          const existingAccount = await accountService.findAccount(accountId);
+          if (!existingAccount) {
+               // error: 계정이 존재하지 않는 경우
+               res.status(400).json({ message: "계정이 존재 하지 않습니다." });
+          }
 
-    const isValidPassword = await bcrypt.compare(password, existingAccount.password);
-    if (!isValidPassword) {
-      throwError(401, '잘못된 비밀번호');
-    }
+          const isValidPassword = await bcrypt.compare(
+               password,
+               existingAccount.password
+          );
+          if (!isValidPassword) {
+               throwError(401, "잘못된 비밀번호");
+          }
 
-    // JWT 토큰, secret-key는 나중에 수정해야 함
-    const token = jwt.sign({ accountId: existingAccount.id }, process.env.SESSION_SECRET_KEY, {
-      expiresIn: '1h',
-    });
+          // JWT 토큰, secret-key는 나중에 수정해야 함
+          const token = jwt.sign(
+               { accountId: existingAccount.id },
+               process.env.SESSION_SECRET_KEY,
+               {
+                    expiresIn: "1h",
+               }
+          );
 
-    res.header('authorization', `Bearer ${token}`);
+          res.header("authorization", `Bearer ${token}`);
 
-    // 쿠키에 저장 refresh 토큰, 이 부분은 필요없으면 삭제 할 것
-    // 사용하려면 재발급 관련 코드 짤 것
-    // const refreshToken = jwt.sign({ accountId: existingAccount.id }, 'refresh-key', {
-    //   expiresIn: '7d',
-    // });
+          // 쿠키에 저장 refresh 토큰, 이 부분은 필요없으면 삭제 할 것
+          // 사용하려면 재발급 관련 코드 짤 것
+          // const refreshToken = jwt.sign({ accountId: existingAccount.id }, 'refresh-key', {
+          //   expiresIn: '7d',
+          // });
 
-    // res.cookie('refreshToken', refreshToken, {
-    //   httpOnly: true,
-    //   secure: true,
-    //   maxAge: 7 * 24 * 60 * 60 * 1000,
-    // });
+          // res.cookie('refreshToken', refreshToken, {
+          //   httpOnly: true,
+          //   secure: true,
+          //   maxAge: 7 * 24 * 60 * 60 * 1000,
+          // });
 
-    return res.status(200).json({ message: '로그인에 성공' });
-  } catch (error) {
-    next(error);
-  }
+          return res
+               .status(201)
+               .json({ message: "로그인 성공", account: accountId });
+     } catch (error) {
+          next(error);
+     }
 }
--- server/services/accountService.js
+++ server/services/accountService.js
@@ -1,34 +1,44 @@
-import { prisma } from '../utils/prisma/index.js';
-import bcrypt from 'bcrypt';
+import { prisma } from "../utils/prisma/index.js";
+import bcrypt from "bcrypt";
 
 class Account {
-  static instance = null;
-  constructor(prisma) {
-    if (Account.instance) {
-      return Account.instance;
-    }
+     static instance = null;
+     constructor(prisma) {
+          if (Account.instance) {
+               return Account.instance;
+          }
 
-    this.prisma = prisma;
-    Account.instance = this;
-  }
+          this.prisma = prisma;
+          Account.instance = this;
+     }
 
-  async createAccount(accountId, password, uuid) {
-    const hashedPassword = await bcrypt.hash(password, 10);
+     async createAccount(accountId, password, uuid) {
+          const hashedPassword = await bcrypt.hash(password, 10);
 
-    const account = await prisma.accounts.create({
-      data: { accountId: accountId, password: hashedPassword, uuid: uuid },
-    });
-    return account;
-  }
+          const account = await prisma.accounts.create({
+               data: {
+                    accountId: accountId,
+                    password: hashedPassword,
+                    uuid: uuid,
+               },
+          });
 
-  async findAccount(accountId) {
-    const account = await prisma.accounts.findUnique({
-      where: { accountId },
-    });
+          const scoreData = await prisma.score.create({
+               data: {
+                    accountId: accountId,
+               },
+          });
+          return account;
+     }
 
-    return account;
-  }
+     async findAccount(accountId) {
+          const account = await prisma.accounts.findUnique({
+               where: { accountId },
+          });
+
+          return account;
+     }
 }
 
-// const account = new Account(prisma);
-// export default account;
+const account = new Account(prisma);
+export default account;
