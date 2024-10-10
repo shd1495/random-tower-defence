2cf194954d22a013043ef7301b6e62edede0d717
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-10 17:47:41 +0900

    (jyp) fix: "비밀번호 2번 해싱 1번으로 수정"

--- server/services/accountService.js
+++ server/services/accountService.js
@@ -1,43 +1,40 @@
-import { prisma } from "../utils/prisma/index.js";
-import bcrypt from "bcrypt";
+import { prisma } from '../utils/prisma/index.js';
 
 class Account {
-     static instance = null;
-     constructor(prisma) {
-          if (Account.instance) {
-               return Account.instance;
-          }
+  static instance = null;
+  constructor(prisma) {
+    if (Account.instance) {
+      return Account.instance;
+    }
 
-          this.prisma = prisma;
-          Account.instance = this;
-     }
+    this.prisma = prisma;
+    Account.instance = this;
+  }
 
-     async createAccount(accountId, password, uuid) {
-          const hashedPassword = await bcrypt.hash(password, 10);
+  async createAccount(accountId, password, uuid) {
+    const account = await prisma.accounts.create({
+      data: {
+        accountId: accountId,
+        password: password,
+        uuid: uuid,
+      },
+    });
 
-          const account = await prisma.accounts.create({
-               data: {
-                    accountId: accountId,
-                    password: hashedPassword,
-                    uuid: uuid,
-               },
-          });
+    const scoreData = await prisma.score.create({
+      data: {
+        accountId: accountId,
+      },
+    });
+    return account;
+  }
 
-          const scoreData = await prisma.score.create({
-               data: {
-                    accountId: accountId,
-               },
-          });
-          return account;
-     }
+  async findAccount(accountId) {
+    const account = await prisma.accounts.findUnique({
+      where: { accountId },
+    });
 
-     async findAccount(accountId) {
-          const account = await prisma.accounts.findUnique({
-               where: { accountId },
-          });
-
-          return account;
-     }
+    return account;
+  }
 }
 
 const account = new Account(prisma);
