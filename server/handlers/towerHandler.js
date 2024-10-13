9554936b06b8456e3e8eb9081414d380def0325a
JamGwang <twr9680@naver.com>
2024-10-13 17:37:23 +0900

    fix 송재광 타워 업그레이드 에러처리 try catch 수정

--- server/handlers/towerHandler.js
+++ server/handlers/towerHandler.js
@@ -114,41 +114,43 @@ export const towerSell = async (uuid, payload) => {
 
 // 타워 업그레이드
 export const towerUpgrade = async (uuid, payload) => {
+
   const { towers } = getGameAssets();
   const { tower, beforeUniqueId, afterUniqueId, userGold, posX, posY } = payload;
 
-  // 타워id 검증
-  const isExistTower = towers.data.find((t) => tower.id === t.id);
-  if (!isExistTower) return { type: 'upgradeTower', status: 'fail', message: 'Invalid tower ID' };
+  try {
+    // 타워id 검증
+    const isExistTower = towers.data.find((t) => tower.id === t.id);
+    if (!isExistTower) throw new Error('Invalid tower ID');
 
-  // 골드 검증
-  if (userGold < isExistTower.upgradePrice)
-    return { type: 'upgradeTower', status: 'fail', message: `be short on one's gold` };
+    // 골드 검증
+    if (userGold < isExistTower.upgradePrice)
+      throw new Error('be short gold');
 
-  // 강화 단계 검증    
-  const isExistNextGrade = towers.data.find((t) => isExistTower.nextGradeId === t.id);
-  if (isExistNextGrade === -1 || !isExistNextGrade)
-    return { type: '', status: 'fail', message: 'tower is already max grade or Invalid next grade' };
-  // 검증 모두 성공하면 
-  const nextGradeTower = towers.data.find((t) => t.id === isExistNextGrade.id);
-  if (!nextGradeTower) return { type: 'upgradeTower', status: 'fail', message: 'Invalid next grade tower ID' };
-  await updateUserGold(uuid, -isExistTower.upgradePrice);
-  const userGoldData = await getUserGold(uuid);
-  console.log('업글타워id' + afterUniqueId);
-  await upgradeTower(uuid, beforeUniqueId, afterUniqueId, nextGradeTower, posX, posY);
+    // 강화 단계 검증    
+    const isExistNextGrade = towers.data.find((t) => isExistTower.nextGradeId === t.id);
+    if (isExistNextGrade === -1 || !isExistNextGrade)
+      throw new Error('tower is already max grade or Invalid next grade');
+    const nextGradeTower = towers.data.find((t) => t.id === isExistNextGrade.id);
+    if (!nextGradeTower) throw new Error('Invalid next grade tower ID');
+    // 검증 모두 성공하면
+    await updateUserGold(uuid, -isExistTower.upgradePrice);
+    const userGoldData = await getUserGold(uuid);
+    await upgradeTower(uuid, beforeUniqueId, afterUniqueId, nextGradeTower, posX, posY);
 
-  return {
-    type: 'upgradeTower',
-    status: 'success',
-    message: 'Upgrade Tower successfully',
-    result: {
-      beforeUniqueId: beforeUniqueId, // 삭제할 이전 타워 클라 고유값      
-      // redis 와 같은 key-value의 result 값
-      uniqueId: afterUniqueId, // 새로 배치할 타워 클라 고유값
-      tower: nextGradeTower,  //업그레이드 된 타워 오브잭트 정보
-      posX: posX, // xy 좌표
-      posY: posY,
-      userGold: userGoldData, // 골드 동기화
-    },
-  };
+    return {
+      type: 'upgradeTower',
+      status: 'success',
+      message: 'Upgrade Tower successfully',
+      result: {
+        beforeUniqueId: beforeUniqueId, // 삭제할 이전 타워 클라 고유값      
+        // redis 와 같은 key-value의 result 값
+        uniqueId: afterUniqueId, // 새로 배치할 타워 클라 고유값
+        tower: nextGradeTower,  //업그레이드 된 타워 오브잭트 정보
+        posX: posX, // xy 좌표
+        posY: posY,
+        userGold: userGoldData, // 골드 동기화
+      },
+    };
+  } catch (error) { throw new Error(`타워 업그레이드 에러 ${error.message}`) }
 };
