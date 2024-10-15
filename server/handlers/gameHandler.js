20c64900666e415d80607225e1b55b33d5bc0f1b
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-15 14:41:25 +0900

    (jyp) fix: "최고 점수 예외처리 추가"
    기록이 없을 때 오류가 뜨는 현상 수정

--- server/handlers/gameHandler.js
+++ server/handlers/gameHandler.js
@@ -19,7 +19,7 @@ export const gameStart = async (uuid, payload, socket) => {
     await setWaveLevel(uuid, waveLevel.data[0].id);
 
     const highScore = await scoreService.getHighScore(uuid);
-    if (!highScore)
+    if (!highScore && highScore !== 0)
       return { status: 'fail', type: 'gameStart', message: 'can not read highScore.' };
 
     const gameData = await getGameData(uuid);
