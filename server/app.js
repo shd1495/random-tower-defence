f79ae72ab24dc84a6aae83a6f396cdc785975376
Jinsu_Mun <answlstn13@gmail.com>
2024-10-15 14:38:46 +0900

    문진수 fix 리프레쉬 토큰생성 및 몬스터 생성주기 적용
    로그인 시 리프레쉬 토큰을 쿠키에 생성하게 하였고 클라에서 리프레쉬 토큰이 있으면 엑세스 토큰을 생성하게 하였습니다.
    
    현재 엑세스 토큰은 헤더에 저장하고 있어 만료 시간은 10분으로 짧게 설정했습니다.
    
    몬스터생성 주기는 웨이브 레벨상승시 데이터를 받아와서 기존에 실행되있던 setInterval함수를 중지하고 다시 실행하여 몬스터생성 주기를 변경하였습니다.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-import accountRouter from "./routes/accountRouter.js";
-import express from "express";
-import initSocket from "./init/socket.js";
-import dotenv from "dotenv";
-import { createServer } from "http";
-import { loadGameAssets } from "./init/assets.js";
-import errorHandleMiddleware from "./middlewares/errorHandleMiddleware.js";
+import accountRouter from './routes/accountRouter.js';
+import express from 'express';
+import initSocket from './init/socket.js';
+import dotenv from 'dotenv';
+import { createServer } from 'http';
+import { loadGameAssets } from './init/assets.js';
+import errorHandleMiddleware from './middlewares/errorHandleMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -13,23 +14,24 @@ const PORT = process.env.SECRET_PORT;
 const app = express();
 const server = createServer(app);
 
+app.use(cookieParser());
 // body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("client/public"));
+app.use(express.static('client/public'));
 
-app.use("/api", accountRouter);
+app.use('/api', accountRouter);
 // 소켓 초기화
 initSocket(server);
 app.use(errorHandleMiddleware);
 
 server.listen(PORT, async () => {
-     console.log(`${PORT} 포트로 서버가 열렸습니다.`);
+  console.log(`${PORT} 포트로 서버가 열렸습니다.`);
 
-     try {
-          const assets = await loadGameAssets();
-          console.log("assets 파일이 정상적으로 로드되었습니다.");
-     } catch (error) {
-          console.error("assets 파일을 불러오는 데 실패했습니다.", error);
-     }
+  try {
+    const assets = await loadGameAssets();
+    console.log('assets 파일이 정상적으로 로드되었습니다.');
+  } catch (error) {
+    console.error('assets 파일을 불러오는 데 실패했습니다.', error);
+  }
 });
